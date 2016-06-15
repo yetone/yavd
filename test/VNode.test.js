22f82efafb9d45ea4355ee4cf35f8c669d4aa7f3
guanxipeng <guanxipeng@douban.com>
2016-06-15 21:12:29 +0800

    drive ava

--- test/VNode.test.js
+++ test/VNode.test.js
@@ -2,8 +2,31 @@
  * Created by yetone on 16/6/14.
  */
 
-import VNode from 'VNode';
+import test from 'ava';
+import VNode from '../lib/VNode';
+import { transformToVNode } from '../lib/VNode/utils';
 
-describe('VNode', () => {
-
+test('VNode', t => {
+    let div = document.createElement('div');
+    let html = `
+        <div class="input">
+            <textarea id="html"></textarea>
+        </div>
+        <div id="main">
+            <img>
+            <!--comment-->
+            <img/>
+            <img />
+            I am text
+            <meta />
+            <div>
+                <h1>I am h1</h1>
+                <p>I am p</p>
+            </div>
+        </div>
+    `;
+    div.innerHTML = html;
+    let vNode = transformToVNode(div);
+    let vNode1 = transformToVNode(`<div>${html}</div>`);
+    t.deepEqual(vNode, vNode1, 'should equal');
 });
--- test/setup.js
+++ test/setup.js
@@ -6,6 +6,7 @@ import { jsdom } from 'jsdom';
 
 global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
-
-global.expect = require('chai').expect;
-global.should = require('chai').should();
+global.Node = window.Node;
+global.HTMLElement = window.HTMLElement;
+global.Text = window.Text;
+global.Comment = window.Comment;
